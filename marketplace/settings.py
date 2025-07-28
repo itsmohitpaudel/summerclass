dd423692e1f4166f8620b4c4952aa37d8025ebd5
Mohit Paudel <67942667+itsmohitpaudel@users.noreply.github.com>
2025-07-28 22:50:15 +0545

    Added CSRF_TRUSTED

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -154,6 +154,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://summerclass-crjx.onrender.com',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
